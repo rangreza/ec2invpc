1f11fe71fa1e59dea8b5300dd8dd26b07f34f7b9
rangreza <81700083+rangreza@users.noreply.github.com>
2024-11-12 15:26:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,3 +51,23 @@ resource "aws_instance" "private_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private.id
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "public_instance_id" {
+  value = aws_instance.public_instance.id
+}
+
+output "private_instance_id" {
+  value = aws_instance.private_instance.id
+}
